bf73e750004b1c1d5c1322c8ea986a4eb81e57ca
chcv1234 <chcv1234@gmail.com>
2019-04-19 18:30:01 +0800

    3rd
    3rd upload
    js : selected and begray complete

--- script.js
+++ script.js
@@ -2,3 +2,39 @@
 
 // You can access the RESULTS_MAP from "constants.js" in this file since
 // "constants.js" has been included before "script.js" in index.html.
+
+const items = document.querySelectorAll('.flex-item')
+var allItem = {} ;
+
+for(const item of items)
+{
+    item.addEventListener('click',beSelect);
+
+    allItem[item.dataset.choiceId+item.dataset.questionId] = {
+        type:item.dataset.choiceId+item.dataset.questionId,
+        CID:item.dataset.choiceId,
+        QID:item.dataset.questionId,
+        selected:false
+    }
+}
+
+function beSelect(event) {
+    const container = event.currentTarget;
+    container.classList.add('flex-item-selected');
+    allItem[container.dataset.choiceId+container.dataset.questionId].selected = true ;
+    beGray();
+}
+
+function beGray() {
+
+    console.log("be gray");
+    for(let item of items)
+    {
+        let thisType=item.dataset.choiceId+item.dataset.questionId;
+
+        if(allItem[thisType].selected===false)
+        {
+            item.classList.add('flex-item-unselected');
+        }
+    }
+}
